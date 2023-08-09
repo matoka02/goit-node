56f391f78887e406d15bccee8722cee61766ac26
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-09 20:33:59 +0300

    Create app.js

---
+++ lesson-02-003/express-example-1/app.js
@@ -0,0 +1,24 @@
+// вызов библиотеки
+const express = require("express");
+
+// создание веб-сервера
+const app = express();
+
+// // запуск сервера - не указывать 3000, если фронт на нем
+// app.listen(4000, () => console.log("Server running"));
+
+app.get('/', (request, responce)=>{
+  responce.send(`<h2>Home page</h2>`)
+});
+
+app.get('/contacts', (request, responce)=>{
+  console.log(request.url);
+  console.log(request.method);
+  responce.send(`<h2>Contacts page</h2>`)
+});
+
+app.listen(3000, () => console.log("Server running"));
+
+
+
+
