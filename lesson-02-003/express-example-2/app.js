e6f6a8b3fd335800d201054aa3ce5c2f3b32f6b7
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-10 13:25:28 +0300

    Update app.js

--- lesson-02-003/express-example-2/app.js
+++ lesson-02-003/express-example-2/app.js
@@ -5,7 +5,13 @@ const app = express();
 const books = require('./books');
 
 app.get('/books', (req, resp) => {
-  resp.send(books)
+  // resp.send(books);
+  resp.json(books);
+
+  // метод send не передает значение null, только json
+  // const databasePesponce = null;
+  // resp.send(databasePesponce);
+  // resp.json(databasePesponce);
 })
 
-app.listen(3000);
+app.listen(4000);
