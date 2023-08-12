dac74921f5b92eadba8aa0e2f96bf22de8d855b4
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-12 12:33:44 +0300

    Update books.js

--- lesson-02-004/controllers-decorates-middlewares-example/controllers/books.js
+++ lesson-02-004/controllers-decorates-middlewares-example/controllers/books.js
@@ -1,55 +1,51 @@
-
-
-const books = require("../models/books")
+const books = require("../models/books");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
+const getAll = async (req, resp) => {
+  const respult = await books.getAll();
+  resp.json(respult);
+};
 
+const getById = async (req, resp) => {
+  const { id } = req.params;
+  const respult = await books.getById(id);
+  if (!respult) {
+    throw HttpError(404, "Not found");
+  }
+  resp.json(respult);
+};
 
-const getAll = async (req, res) => {
-    const result = await books.getAll();
-    res.json(result);
-}
+const add = async (req, resp) => {
+  const respult = await books.add(req.body);
+  resp.status(201).json(respult);
+};
 
-const getById = async (req, res) => {
-    const { id } = req.params;
-    const result = await books.getById(id);
-    if (!result) {
-        throw HttpError(404, "Not found");
-    }
-    res.json(result);
-}
+const updateById = async (req, resp) => {
+  const { id } = req.params;
+  const respult = await books.updateById(id, req.body);
+  if (!respult) {
+    throw HttpError(404, "Not found");
+  }
+  resp.json(respult);
+};
 
-const add = async (req, res) => {
-    const result = await books.add(req.body);
-    res.status(201).json(result);
-}
-
-const updateById = async (req, res) => {
-    const { id } = req.params;
-    const result = await books.updateById(id, req.body);
-    if (!result) {
-        throw HttpError(404, "Not found");
-    }
-    res.json(result);
-}
-
-const deleteById = async (req, res) => {
-    const { id } = req.params;
-    const result = await books.deleteById(id);
-    if (!result) {
-        throw HttpError(404, "Not found");
-    }
-    // res.status(204).send()
-    res.json({
-        message: "Delete success"
-    })
-}
+const deleteById = async (req, resp) => {
+  const { id } = req.params;
+  const respult = await books.deleteById(id);
+  if (!respult) {
+    throw HttpError(404, "Not found");
+  }
+  // resp.status(204).send()
+  resp.json({
+    message: "Delete success",
+  });
+};
 
 module.exports = {
-    getAll: ctrlWrapper(getAll),
-    getById: ctrlWrapper(getById),
-    add: ctrlWrapper(add),
-    updateById: ctrlWrapper(updateById),
-    deleteById: ctrlWrapper(deleteById),
-}
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  add: ctrlWrapper(add),
+  updateById: ctrlWrapper(updateById),
+  deleteById: ctrlWrapper(deleteById),
+};
