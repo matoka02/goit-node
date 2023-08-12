a3dfc0e3e3eacd7eeb8d8f55ba98c466d05dedf4
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-12 12:39:48 +0300

    Update books.js

--- lesson-02-004/controllers-decorates-middlewares-example/controllers/books.js
+++ lesson-02-004/controllers-decorates-middlewares-example/controllers/books.js
@@ -1,39 +1,136 @@
 const books = require("../models/books");
 
+// const { HttpError } = require('../helpers');
 const { HttpError, ctrlWrapper } = require("../helpers");
 
+// рефакторинг, вынесено в /shemas/books.js
+// const Joi = require("joi");
+// const addSchema = Joi.object({
+//   title: Joi.string().required(),
+//   author: Joi.string().required(),
+// });
+
+// const getAll = async (req, resp) => {
+//   try {
+//     const result = await books.getAll();
+//     resp.json(result);
+//   } catch (error) {
+//     // next(error)
+//   }
+// };
+
+// const getById = async (req, resp, next) => {
+//   try {
+//     const { id } = req.params;
+//     const result = await books.getById(id);
+
+//     if (!result) {
+//       throw HttpError(404, 'Not found')
+//     };
+
+//     resp.json(result);
+//   } catch (error) {
+//     next(error)
+//   }
+// };
+
+// const add = async (req, resp, next) => {
+//   try {
+//     const { error } = addSchema.validate(req.body);
+//     if (error) {
+//       throw HttpError(400, error.message)
+//     };
+//     const result = await books.add(req.body);
+//     resp.status(201).json(result)
+//   } catch (error) {
+//     next(error)
+//   }
+// };
+
+// const updateById = async (req, resp, next) => {
+//   try {
+//     const { error } = addSchema.validate(req.body);
+//     if (error) {
+//       throw HttpError(400, error.message)
+//     };
+//     const { id } = req.params;
+//     const result = await books.updateById(id, req.body);
+//     if (!result) {
+//       throw HttpError(404, 'Not found')
+//     };
+//     resp.json(result)
+//   } catch (error) {
+//     next(error)
+//   }
+// };
+
+// const deleteById = async (req, resp, next) => {
+//   try {
+//     const { id } = req.params;
+//     const result = await books.deleteById(id);
+//     if (!result) {
+//       throw HttpError(404, 'Not found')
+//     };
+//     resp.json({
+//       message: 'Delete successfully'
+//     });
+//   } catch (error) {
+//     next(error)
+//   }
+// };
+
+/**
+ * 
+ * @param {*} req 
+ * @param {*} resp 
+ */
+
+// рефакторинг, try-catch вынесено отдельной функцией в /helpers/ctrlWrapper.js, фргумент next тоже можно убрать
+
 const getAll = async (req, resp) => {
-  const respult = await books.getAll();
-  resp.json(respult);
+  const result = await books.getAll();
+  resp.json(result);
 };
 
 const getById = async (req, resp) => {
   const { id } = req.params;
-  const respult = await books.getById(id);
-  if (!respult) {
+  const result = await books.getById(id);
+  if (!result) {
     throw HttpError(404, "Not found");
   }
-  resp.json(respult);
+  resp.json(result);
 };
 
 const add = async (req, resp) => {
-  const respult = await books.add(req.body);
-  resp.status(201).json(respult);
+  // // рефакторинг, вынесено в /shemas/books.js
+  // const { error } = addSchema.validate(req.body);
+  // if (error) {
+  //   throw HttpError(400, error.message);
+  // };
+
+  const result = await books.add(req.body);
+  resp.status(201).json(result);
 };
 
 const updateById = async (req, resp) => {
+  // // рефакторинг, вынесено в /shemas/books.js
+  // const { error } = addSchema.validate(req.body);
+  // if (error) {
+  //   throw HttpError(400, error.message);
+  // };
+
   const { id } = req.params;
-  const respult = await books.updateById(id, req.body);
-  if (!respult) {
+  const result = await books.updateById(id, req.body);
+  if (!result) {
     throw HttpError(404, "Not found");
   }
-  resp.json(respult);
+  resp.json(result);
 };
 
 const deleteById = async (req, resp) => {
   const { id } = req.params;
-  const respult = await books.deleteById(id);
-  if (!respult) {
+  const result = await books.deleteById(id);
+  if (!result) {
     throw HttpError(404, "Not found");
   }
   // resp.status(204).send()
