c27365c4b3870ab84e388f3749751b56cdc83875
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-12 23:34:52 +0300

    update

---
+++ lesson-03-005/mongodb-connect-example/app.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const PASSWORD = "8FWnSc0ASo3vOaWX";
+const MY_BASE = "books_reader";
+const DB_HOST = `mongodb+srv://matoka:${PASSWORD}@cluster0.cfo46i6.mongodb.net/${MY_BASE}?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connect success"))
+  .catch((error) => console.error(error.message));
