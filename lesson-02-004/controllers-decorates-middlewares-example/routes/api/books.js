c40cec4bfb6920cfc69d07ff60a93f6f56c13749
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-12 12:27:04 +0300

    Update books.js

--- lesson-02-004/controllers-decorates-middlewares-example/routes/api/books.js
+++ lesson-02-004/controllers-decorates-middlewares-example/routes/api/books.js
@@ -1,19 +1,20 @@
 const express = require("express");
 
 const ctrl = require("../../controllers/books");
-
 const {validateBody} = require("../../middlewares");
-
 const schemas = require("../../schemas/books");
 
 const router = express.Router();
 
+// рефакторинг, все функции вынесены в /controllers/books.js
 router.get("/", ctrl.getAll);
 
 router.get("/:id", ctrl.getById);
 
+// router.post('/', ctrl.add);
 router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
+// router.put('/:id', ctrl.updateById);
 router.put("/:id",  validateBody(schemas.addSchema), ctrl.updateById);
 
 router.delete("/:id", ctrl.deleteById);
