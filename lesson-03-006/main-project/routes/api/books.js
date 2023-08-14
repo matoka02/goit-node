0c5fd7821df97c64626c37039de7f563d8f14769
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-14 13:36:09 +0300

    update

--- lesson-03-006/main-project/routes/api/books.js
+++ lesson-03-006/main-project/routes/api/books.js
@@ -2,21 +2,21 @@ const express = require("express");
 
 const ctrl = require("../../controllers/books");
 
-// const {validateBody} = require("../../middlewares");
+const {validateBody, isValidId} = require("../../middlewares");
 
-// const {schemas} = require("../../models/book");
+const {schemas} = require("../../models/book");
 
 const router = express.Router();
 
 router.get("/", ctrl.getAll);
 
-router.get("/:id", ctrl.getById);
+router.get("/:id", isValidId, ctrl.getById);
 
-// router.post("/", validateBody(schemas.addSchema), ctrl.add);
-router.post("/", ctrl.add);
+router.post("/", validateBody(schemas.addSchema), ctrl.add);
+// router.post("/", ctrl.add);
 
-// router.put("/:id",  validateBody(schemas.addSchema), ctrl.updateById);
+// router.put("/:id", isValidId,  validateBody(schemas.addSchema), ctrl.updateById);
 
-// router.delete("/:id", ctrl.deleteById);
+// router.delete("/:id", isValidId, ctrl.deleteById);
 
 module.exports = router;
