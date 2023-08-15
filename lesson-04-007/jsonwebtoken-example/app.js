81764276d87e3066f0c1ea3f550357fb4d085fa2
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-15 21:13:52 +0300

    update

--- lesson-04-007/jsonwebtoken-example/app.js
+++ lesson-04-007/jsonwebtoken-example/app.js
@@ -1,24 +1,28 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-const {SECRET_KEY} = process.env;
+const { SECRET_KEY } = process.env;
 
 const payload = {
-    id: "63fe4a5a68b27c947e28495b"
-}
+  id: "64db6362b56cdabe515879c9",
+};
 
-const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"});
-// console.log(token);
+// создание токена, указаны id, ключ и срок жизни токена
+const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+console.log(token);     
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZGI2MzYyYjU2Y2RhYmU1MTU4NzljOSIsImlhdCI6MTY5MjEyMjQ2OCwiZXhwIjoxNjkyMjA1MjY4fQ.XY0GRBceJXJNuIMzBGxakJbvZ_NwBxyq0uKLbV22eqk
 
 const decodeToken = jwt.decode(token);
 // console.log(decodeToken);
+// { id: '64db6362b56cdabe515879c9', iat: 1692122664, exp: 1692205464 }
 
+// проверка валидности токена
 try {
-    const {id} = jwt.verify(token, SECRET_KEY);
-    console.log(id);
-    const invalidToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZmU0YTVhNjhiMjdjOTQ3ZTI4NDk1YiIsImlhdCI6MTY3NzYxMDczNSwiZXhwIjoxNjc3NjkzNTM1fQ.YELvZzq9k2ErXHMwufOFfJUtEi2jxl6W5wtG4tiKnxS"
-    const result = jwt.verify(invalidToken, SECRET_KEY)
-}
-catch(error) {
-    console.log(error.message);
+  const { id } = jwt.verify(token, SECRET_KEY);
+  console.log(id);      // 64db6362b56cdabe515879c9
+  const invalidToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZGI2MzYyYjU2Y2RhYmU1MTU4NzljOSIsImlhdCI6MTY5MjEyMjQ2OCwiZXhwIjoxNjkyMjA1MjY4fQ.XY0GRBceJXJNuIMzBGxakJbvZ_NwBxyq0uKLbV22eqk";
+  const result = jwt.verify(invalidToken, SECRET_KEY);
+} catch (error) {
+  console.log(error.message);     // invalid signature
 }
