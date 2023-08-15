a19824ecfe523698e0dd04d827b3527aa2bb6d39
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-15 13:07:55 +0300

    delete

--- lesson-04-007/main-project/middlewares/index.js
+++
@@ -1,9 +0,0 @@
-const validateBody = require("./validateBody");
-const isValidId = require("./isValidId");
-const handleMongooseError = require("./handleMongooseError");
-
-module.exports = {
-    validateBody,
-    isValidId,
-    handleMongooseError,
-}
