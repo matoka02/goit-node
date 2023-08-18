215a162efc14750b27e59858f122a14109032766
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-18 18:51:29 +0300

    update

--- lesson-05-009/main-project/middlewares/index.js
+++
@@ -1,9 +0,0 @@
-const validateBody = require("./validateBody");
-const isValidId = require("./isValidId");
-const authenticate = require('./authenticate');
-
-module.exports = {
-    validateBody,
-    isValidId,
-    authenticate
-}
