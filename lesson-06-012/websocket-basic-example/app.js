18d5430ea21c59e223042c2a1d5800bcda03e7c2
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-23 22:32:37 +0300

    update

--- lesson-06-012/websocket-basic-example/app.js
+++ lesson-06-012/websocket-basic-example/app.js
@@ -1,19 +1,24 @@
 const ws = new require("ws");
 
-const wsServer = new ws.Server({port: 5000});
+// создание веб-сокет сервера
+const wsServer = new ws.Server({ port: 5000 });
 
+// запись всех подключенных сокетов
 const socketList = [];
 
-wsServer.on("connection", (socket)=> {
-    socketList.push(socket);
-    console.log(socketList)
-    setTimeout(()=> {
-        socket.send("Welcome to web-socket server");
-    }, 3000)
-    // console.log("New frontend connected");
-    socketList.forEach(item => {
-        if(item !== socket) {
-            item.send("New member connect")
-        }
-    })
+// слушатель подключений
+wsServer.on("connection", (socket) => {
+  // console.log("New frontend connected");
+  socketList.push(socket);
+  // console.log(socketList);
+  // сообщение только для нового подключения
+  setTimeout(() => {
+    socket.send("Welcome to web-socket server");
+  }, 4000);
+  // сообщение для остальных подключений
+  socketList.forEach((item) => {
+    if (item !== socket) {
+      item.send("New member connect");
+    }
+  });
 });
