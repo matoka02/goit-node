c6a8a7e5adbbbdd815a6aad1e5917970a9574d69
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-23 20:18:28 +0300

    add sendEmail

--- lesson-06-011/main-project/models/user.js
+++ lesson-06-011/main-project/models/user.js
@@ -29,6 +29,14 @@ const userSchema = new Schema({
       type: String,
       required: true,
     },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationCode: {
+      type: String,
+      default: '',
+    },
   },
   { versionKey: false, timestamps: true }
 );
