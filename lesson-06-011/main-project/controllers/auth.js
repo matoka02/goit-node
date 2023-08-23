e2d3e6af399182bf09034aad392ac71243703457
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-23 20:33:04 +0300

    update

--- lesson-06-011/main-project/controllers/auth.js
+++ lesson-06-011/main-project/controllers/auth.js
@@ -3,12 +3,13 @@ const jwt = require("jsonwebtoken");
 const gravatar = require('gravatar');
 const path = require("path");
 const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
 
 const { User } = require("../models/user");
 
-const { HttpError, ctrlWrapper } = require("../helpers");
+const { HttpError, ctrlWrapper, sendEmail } = require("../helpers");
 
-const { SECRET_KEY } = process.env;
+const { SECRET_KEY, BASE_URL } = process.env;
 
 const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
@@ -21,10 +22,18 @@ const register = async (req, resp) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
-
   const avatarURL = gravatar.url(email);
+  const verificationCode = nanoid();
 
-  const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
+  const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL, verificationCode });
+
+  const verifyEmail = {
+    to: email,
+    subject: 'Verify email',
+    html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${verificationCode}">Click verify email</a>`
+  };
+
+  await sendEmail(verifyEmail);
 
   resp.status(201).json({
     email: newUser.email,
