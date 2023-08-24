cf47624f6b70d568f079fee55a905a0b46848134
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-08-24 20:47:56 +0300

    update

--- lesson-06-012/socket.io-chat-example/server/app.js
+++ lesson-06-012/socket.io-chat-example/server/app.js
@@ -1,18 +1,23 @@
+// постройка сервера на основе протокола HTTP
 const { Server } = require("socket.io");
 const { createServer } = require("http");
 
+// создание пустого сервера
 const httpServer = createServer();
 
+// создание веб-сокета, разрешено подкоючение с любых других портов
 const io = new Server(httpServer, {
-    cors: {
-        origin: "*"
-    }
+  cors: {
+    origin: "*",
+  },
 });
 
+// слушатель
 io.on("connection", (socket) => {
-    socket.on("chat-message", message => {
-        socket.broadcast.emit("chat-message", message);
-    })
+  console.log('New frontend connect');
+  // socket.on("chat-message", (message) => {
+    // socket.broadcast.emit("chat-message", message);
+  // });
 });
 
-httpServer.listen(3001);
+httpServer.listen(4001);
