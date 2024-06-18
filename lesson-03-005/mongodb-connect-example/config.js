512187e9ca6a934e9428858e984ce53addf23d31
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-06-18 19:20:49 +0300

    update claster in MongoDB

--- lesson-03-005/mongodb-connect-example/config.js
+++ lesson-03-005/mongodb-connect-example/config.js
@@ -1,3 +1,3 @@
-const DB_HOST = `mongodb+srv://matoka:8FWnSc0ASo3vOaWX@cluster0.cfo46i6.mongodb.net/books_reader?retryWrites=true&w=majority`;
+const DB_HOST = `mongodb+srv://matoka:8FWnSc0ASo3vOaWX@cluster0.8v73tny.mongodb.net/books_reader?retryWrites=true&w=majority`;
 
 module.exports = {DB_HOST};
