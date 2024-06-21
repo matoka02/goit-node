959d0b60b691fadfa43dcb6cbd4482c2619d3230
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-06-21 13:14:15 +0300

    update email-address

--- lesson-06-011/nodemailer/app.js
+++ lesson-06-011/nodemailer/app.js
@@ -1,16 +1,23 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-const { META_PASSWORD } = process.env;
+const { META_PASSWORD, SSL_PASSWORD } = process.env;
 
 // создание объекта настроек для удаленного доступа через протокол POP3/SMTP
 const nodemailerConfig = {
-  host: 'smtp.meta.ua',
-  port: 465, // 25, 2525 - это порты meta.ua
+  // host: 'smtp.meta.ua',
+  // port: 465, // 25, 2525 - это порты meta.ua
+  // secure: true,
+  // auth: {
+  //   user: 'tessa02@meta.ua',
+  //   pass: META_PASSWORD,
+  // },
+  host: 'smtp.gmail.com',
+  port: 465,
   secure: true,
   auth: {
-    user: 'tessa02@meta.ua',
-    pass: META_PASSWORD,
+    user: 'kurama.sama.024@gmail.com',
+    pass: SSL_PASSWORD,
   },
 };
 
@@ -18,12 +25,12 @@ const transport = nodemailer.createTransport(nodemailerConfig);
 
 // создание объекта настроек письма
 const email = {
-  to: "fixig95064@backva.com",
-  from: "tessa02@meta.ua",
+  to: "tahogeg206@elahan.com",
+  from: "kurama.sama.024@gmail.com",
   subject: "Test email",
   html: "<p><strong>Test email</strong> from localhost:4000</p>",
 };
 
 transport.sendMail(email)
-    .then(()=> console.log("Email send success"))
-    .catch(error => console.log(error.message));
+  .then(() => console.log("Email send success"))
+  .catch(error => console.log(error.message));
