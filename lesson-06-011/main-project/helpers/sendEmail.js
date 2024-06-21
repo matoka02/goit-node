a0b754b77bf431f722dddbb34945a672ce280916
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-06-21 13:15:14 +0300

    add nodemailer

--- lesson-06-011/main-project/helpers/sendEmail.js
+++ lesson-06-011/main-project/helpers/sendEmail.js
@@ -1,14 +1,37 @@
-const sgMail = require("@sendgrid/mail");
+// const sgMail = require("@sendgrid/mail");
+const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-const { SENDGRID_API_KEY } = process.env;
+// const { SENDGRID_API_KEY } = process.env;
 
-sgMail.setApiKey(SENDGRID_API_KEY);
+// sgMail.setApiKey(SENDGRID_API_KEY);
 
-// функция-посредник для отправки
+// // функция-посредник для отправки
+// const sendEmail = async (data) => {
+//   const email = { ...data, from: "kurama.sama.024@gmail.com" };
+//   await sgMail.send(email);
+//   return true;
+// };
+
+// module.exports = sendEmail;
+
+const { SSL_PASSWORD } = process.env;
+
+const nodemailerConfig = {
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'kurama.sama.024@gmail.com',
+    pass: SSL_PASSWORD,
+  },
+};
+
+const transport = nodemailer.createTransport(nodemailerConfig);
+
 const sendEmail = async (data) => {
   const email = { ...data, from: "kurama.sama.024@gmail.com" };
-  await sgMail.send(email);
+  await transport.sendMail(email);
   return true;
 };
 
